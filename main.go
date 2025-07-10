53c5649d62edcdfbad3a75bc661889146a5aad62
faizanfirdousi <faizanfirdousi.official@gmail.com>
2025-07-10 21:59:33 +0530

    improved, can add arguments and do cd

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
-package main
+package main 
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -14,29 +15,59 @@ func main() {
 
 	for {
 		fmt.Print(">> ")
+
+		//Read the keyboard input
 		input, err := reader.ReadString('\n')
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
 
-		//handle the execution of the input
+
+		//Handle the execution of the input
 		if err = execInput(input); err != nil {
-			fmt.Fprintln(os.Stderr,err)
+			fmt.Fprintln(os.Stderr, err)
 		}
 	}
 }
 
+
+//ErrNoPath is returned when 'cd' was called without a second argument
+
+var ErrNoPath = errors.New("path required")
+
 func execInput(input string) error {
-	//remove the newline character
+	// Remove the newline character
 	input = strings.TrimSuffix(input, "\n")
 
-	//prepare the command to execute
-	cmd := exec.Command(input)
 
-	//set the correct output device
+	//Split the input separate the command and the arguments
+	args := strings.Split(input, " ")
+
+	// Check for built-in commands 
+	switch args[0] {
+	case "cd":
+		// 'cd' to home with empty path not yet supported
+
+		if len(args) < 2 {
+			return ErrNoPath
+		}
+
+		// change the directory and return the error
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+
+	//prepare the command to execute
+	cmd := exec.Command(args[0], args[1:]...)
+
+
+	// Set the correct output device 
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 
-	//execute the command and return the error
+	// Execute the command and return the error
 	return cmd.Run()
 }
